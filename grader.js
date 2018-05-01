06643eece40723a06d0dd07e6e127c3ee1bbd8d6
Cédric <cedric.david@btopenworld.com>
2018-05-01 22:59:52 +1000

    added console.log for returns

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
- function average(scores) {
+  function average(scores) {
 	 var sum = 0;
 	 for (i = 0; i < scores.length; i++) {
 		 sum += scores[i];		 
@@ -6,9 +6,8 @@
 	 return Math.round(sum/scores.length);
  }
  
- var scores = [ 90, 98, 89 10, 10, 86, 94];
- average(scores);
+ var scores = [ 90, 98, 89, 100, 100, 86, 94];
+ console.log(average(scores));
  
  var scores2 = [ 40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
- average(scores);
- 
+ console.log(average(scores2));
